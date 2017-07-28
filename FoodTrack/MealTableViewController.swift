d8e901a0b22f2f2e64e2cbd2ece7436860c4dbd4
johanna.sorbom <sorbom.johanna@gmail.com>
2017-07-28 20:08:01 +0200

    Display the Data

--- FoodTrack/MealTableViewController.swift
+++ FoodTrack/MealTableViewController.swift
@@ -30,6 +30,11 @@ class MealTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         
