6677ed28995bf210ac75494cf4e1b5288d7ad6df
johanna.sorbom <sorbom.johanna@gmail.com>
2017-07-29 14:00:11 +0200

    Implement the prepare(for:sender:) method on MealViewController

--- FoodTrack/MealViewController.swift
+++ FoodTrack/MealViewController.swift
@@ -52,6 +52,29 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     func textFieldDidEndEditing(_ textField: UITextField) {
             }
     
+    //MARK: Navigation
+    
+    // This method lets you configure a view controller before it's presented.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }    
+    
+    
     //MARK: Action
     
    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
