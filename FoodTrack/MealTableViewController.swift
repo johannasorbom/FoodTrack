ebeab3a90fa4432f5be26116b029b612067911d1
johanna.sorbom <sorbom.johanna@gmail.com>
2017-07-29 18:04:30 +0200

    To load the meal list at the appropriate time

--- FoodTrack/MealTableViewController.swift
+++ FoodTrack/MealTableViewController.swift
@@ -20,6 +20,16 @@ class MealTableViewController: UITableViewController {
         
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }
+        
+        else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
 
         // Load the sample data.
         loadSampleMeals()    }
